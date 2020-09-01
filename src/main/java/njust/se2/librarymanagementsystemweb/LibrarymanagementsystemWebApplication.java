1ad65cab6f4085b31756d034e7c57f356c52baa5
njustmengfanyu <1471464620@qq.com>
2020-09-01 20:19:36 +0800

    项目测试用，基础框架SpringBoot的搭建。

---
+++ src/main/java/njust/se2/librarymanagementsystemweb/LibrarymanagementsystemWebApplication.java
@@ -0,0 +1,13 @@
+package njust.se2.librarymanagementsystemweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarymanagementsystemWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarymanagementsystemWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/njust/se2/librarymanagementsystemweb/LibrarymanagementsystemWebApplicationTests.java
@@ -0,0 +1,13 @@
+package njust.se2.librarymanagementsystemweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymanagementsystemWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
