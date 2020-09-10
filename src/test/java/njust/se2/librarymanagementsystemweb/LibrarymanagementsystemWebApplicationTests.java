61482c731f5a758256a95d52d05fca71c756d45d
njustmengfanyu <1471464620@qq.com>
2020-09-10 09:47:49 +0800

    login模板初步建立，可直接运行。

--- src/test/java/njust/se2/librarymanagementsystemweb/LibrarymanagementsystemWebApplicationTests.java
+++ src/test/java/njust/se2/librarymanagementsystemweb/LibrarymanagementsystemWebApplicationTests.java
@@ -1,14 +1,41 @@
 package njust.se2.librarymanagementsystemweb;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+//@SpringBootTest
+//class LibrarymanagementsystemWebApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//
+//    }
+//
+//}
+
+@RunWith(SpringRunner.class)
 
 @SpringBootTest
-class LibrarymanagementsystemWebApplicationTests {
-
+@Component
+public class LibrarymanagementsystemWebApplicationTests {
+    @Autowired
+    DataSource datasource;
     @Test
-    void contextLoads() {
-
+    public void contextLoads() {
+        Connection conn;
+        try {
+            conn = datasource.getConnection();
+            System.err.println(conn);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-
 }
