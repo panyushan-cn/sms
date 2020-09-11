e129b329e3fce9ce497f445814461858fab4dc14
njustmengfanyu <1471464620@qq.com>
2020-09-11 19:48:49 +0800

    已经完成后端数据库验证，登录后可以保存状态无需重新登录。

---
+++ src/main/java/njust/se2/librarymanagementsystemweb/config/MyWebConfigurer.java
@@ -0,0 +1,20 @@
+package njust.se2.librarymanagementsystemweb.config;
+
+import njust.se2.librarymanagementsystemweb.interceptor.LoginInterceptor;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.*;
+
+@SpringBootConfiguration
+public class MyWebConfigurer implements WebMvcConfigurer {
+
+    @Bean
+    public LoginInterceptor getLoginIntercepter() {
+        return new LoginInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(getLoginIntercepter()).addPathPatterns("/**").excludePathPatterns("/index.html");
+    }
+}
--- src/main/java/njust/se2/librarymanagementsystemweb/controller/LoginController.java
+++ src/main/java/njust/se2/librarymanagementsystemweb/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
 
+import javax.servlet.http.HttpSession;
 import java.util.Objects;
 
 @RestController
@@ -19,7 +20,7 @@ public class LoginController {
     @CrossOrigin
     @PostMapping(value = "api/login")
     @ResponseBody
-    public Result login(@RequestBody User requestUser) {
+    public Result login(@RequestBody User requestUser, HttpSession session) {
         // 对 html 标签进行转义，防止 XSS 攻击
         String username = requestUser.getUsername();
         username = HtmlUtils.htmlEscape(username);
@@ -28,15 +29,8 @@ public class LoginController {
         if (user == null) {
             return new Result(400);
         } else {
+            session.setAttribute("user",user);
             return new Result(200);
         }
-
-//        if (!Objects.equals("admin", username) || !Objects.equals("123456", requestUser.getPassword())) {
-//            String message = "账号密码错误";
-//            System.out.println("test");
-//            return new Result(400);
-//        } else {
-//            return new Result(200);
-//        }
     }
 }
